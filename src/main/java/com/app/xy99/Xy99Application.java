ebb68701a2ec2f53f1ba807845007ecbc4d82ba5
10129 <1019533934@qq.com>
2019-03-12 18:07:57 +0800

    新加第三方Tomcat部署启动

--- src/main/java/com/app/xy99/Xy99Application.java
+++ src/main/java/com/app/xy99/Xy99Application.java
@@ -3,12 +3,21 @@ package com.app.xy99;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @MapperScan("com.app.xy99.mapper")
 @SpringBootApplication
-public class Xy99Application {
+public class Xy99Application extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Xy99Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Xy99Application.class, args);
     }
-
 }
